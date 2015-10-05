aaf2f6670532817a301aa4b3ef4721147037f387
Matt Michnal <mattm3646@gmail.com>
2015-10-05 16:30:19 -0600

    Added hello_world.rb to branch, completed tasks.
    [#104937556]

---
+++ numbers_letters_and_variables/hello_world.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+puts 'Hello world'
+
+
+#!/usr/bin/env ruby
+
+seconds_var = 60
+minutes_var = 60
+hours_var = 24
+
+
+puts "There are #{seconds_var} seconds in a minute"
+puts "There are #{minutes_var} minutes in an hour"
+puts "There are #{hours_var} hours in a day"
+puts 'That means there are:'
+puts "#{seconds_var * minutes_var} seconds in an hour,"
+puts "#{seconds_var * hours_var} seconds in an day,"
+puts "#{} seconds in a week"
+puts "That means when you turn 20, you will be #{20 * (365 * (seconds_var * hours_var))} seconds. Make them count!"
