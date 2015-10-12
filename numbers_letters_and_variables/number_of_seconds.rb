b684b299c4ffaf545384ed466869555992c22238
Matt Michnal <mattm3646@gmail.com>
2015-10-11 22:30:21 -0600

    Pulled functions out of interpolation
    [#104937600]

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -1,17 +1,25 @@
 #!/usr/bin/env ruby
 
-seconds_var = 60
-minutes_var = 60
-hours_var = 24
-days_var = 7
+seconds_in_a_min = 60
+minutes_in_an_hour = 60
+hours_in_a_day = 24
+days_in_a_week = 7
+seconds_in_an_hour = seconds_in_a_min * minutes_in_an_hour 
+seconds_in_a_day = (seconds_in_a_min * minutes_in_an_hour) * hours_in_a_day
+seconds_in_a_week = ((seconds_in_a_min * minutes_in_an_hour) * hours_in_a_day) * days_in_a_week 
 
-puts "There are #{seconds_var} seconds in a minute"
-puts "There are #{minutes_var} minutes in an hour"
-puts "There are #{hours_var} hours in a day"
-puts "There are #{days_var} days in a week"
+turn_twenty_age_in_seconds = 20 * (52 * days_in_a_week * ((seconds_in_a_min * minutes_in_an_hour) * hours_in_a_day)) 
+turn_one_hundred_age_in_seconds = 100 * (52 * days_in_a_week * ((seconds_in_a_min * minutes_in_an_hour) * hours_in_a_day))
+
+
+
+puts "There are #{seconds_in_a_min} seconds in a minute"
+puts "There are #{minutes_in_an_hour} minutes in an hour"
+puts "There are #{hours_in_a_day} hours in a day"
+puts "There are #{days_in_a_week} days in a week"
 puts "That means there are:"  
-puts "  #{seconds_var * minutes_var} seconds in an hour,"
-puts "  #{(seconds_var * minutes_var) * hours_var} seconds in a day,"
-puts "  #{((seconds_var * minutes_var) * hours_var) * days_var} seconds in a week.\n"
-puts "That means when you turn 20, you've been alive for #{20 * (52 * (((seconds_var * minutes_var) * hours_var) * days_var))} seconds,"
-puts "and if you make it to 100, you will have lived #{100 * (52 * (((seconds_var * minutes_var) * hours_var) * days_var))} seconds. Make them count!"
+puts "  #{seconds_in_an_hour} seconds in an hour,"
+puts "  #{seconds_in_a_day} seconds in a day,"
+puts "  #{seconds_in_a_week} seconds in a week.\n"
+puts "That means when you turn 20, you've been alive for #{turn_twenty_age_in_seconds} seconds,"
+puts "and if you make it to 100, you will have lived #{turn_one_hundred_age_in_seconds} seconds. Make them count!"
