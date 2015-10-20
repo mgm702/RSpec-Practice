a27c2089f6fa3486f30d6d4db23698f9bdc1f77c
Matt Michnal <mattm3646@gmail.com>
2015-10-20 00:00:55 -0600

    Refactored the methods inside CashRegister Class
    [#105433772]

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -2,18 +2,20 @@
 class CashRegister
   attr_reader :total
   def initialize
-    @total = 0
+    @total = 0.0
   end
 
-  def purchase(user_value)
-    @total += user_value.to_f
-    @total
+  def purchase(amount)
+    @total += amount
   end
 
-  def pay(user_value)
-    @total -= user_value.to_f
+  def pay(amount)
+    @total -= amount
     if @total < 0
       puts "Your change is $#{@total.abs.round(2)}"
+      @total = 0
+    elsif @total == 0
+      puts 'You paid the exact amount, there is no change to give.'
       @total = 0
     else
       puts "Your new total is $#{@total.round(2)}"
